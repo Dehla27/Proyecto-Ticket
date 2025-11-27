4f1099fdefad92e36b29568aab09ad6419a46003
Jhorel Jhojan Herrera <jhorel.herrera@impronet.bo>
2025-11-27 12:35:42 -0400

    Fix: jwt auth

--- backend/src/main/java/com/impro/backend/model/Ticket.java
+++ backend/src/main/java/com/impro/backend/model/Ticket.java
@@ -25,7 +25,7 @@ public class Ticket {
     private String numeroTicket;
 
     @Column(name = "descripcion")
-    private Integer descripcion;
+    private String descripcion;
 
     @Column(name = "estado",length = 50)
     private String estado;
